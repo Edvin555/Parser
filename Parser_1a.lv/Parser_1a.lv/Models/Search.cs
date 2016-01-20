af887a1fdcf369df7be31c616dd63669ef745000
Edvins Japins <gelion77@inbox.lv>
2016-01-20 19:06:32 +0200

    initial

--- Parser_1a.lv/Parser_1a.lv/Models/Search.cs
+++ Parser_1a.lv/Parser_1a.lv/Models/Search.cs
@@ -13,6 +13,7 @@ namespace Parser_1a.lv.Models
         [Required(ErrorMessage = "Field is required")]
         [StringLength(20, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         [RegularExpression(@"(?!.*<[^>]*).*", ErrorMessage = "Sorry html tags are not allowed")]
+        [Display(Name = "query string")]
         public string SearchString {get; set;}
     }
 }
